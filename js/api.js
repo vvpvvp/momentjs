15f2d7851a882b9af665aecbe604b4b71dcf744c
vvpvvp <alias8516@sina.com.cn>
2016-08-16 17:51:47 +0800

    let changeto var

--- js/api.js
+++ js/api.js
@@ -1,12 +1,12 @@
 (function(window,$) {
 	$(function(){
-		let doc_url = "https://raw.githubusercontent.com/vvpvvp/momentjs/master/README.md";
+		var doc_url = "https://raw.githubusercontent.com/vvpvvp/momentjs/master/README.md";
 		$.get(doc_url, function(data) {
-			let _content = $(marked(data)),_right = $("#right");
+			var _content = $(marked(data)),_right = $("#right");
             _right.append(_content);
 			$("h1:first,h2:first",_right).remove();
 			$("p",_right).slice(0,2).remove();
-            let lis = $("<ul>");
+            var lis = $("<ul>");
 
             $("#left").append(lis);
             // 完成代码高亮
@@ -14,11 +14,11 @@
               Prism.highlightElement(this);
             });
 
-            let titles = $("h1,h2,h3,h4",_right);
+            var titles = $("h1,h2,h3,h4",_right);
             titles.each(function(i,n){
-            	let _n = $(n);
+            	var _n = $(n);
                   n.id = _n.text();
-            	let li = $("<li class='"+n.tagName+"'>"+_n.text()+"</li>");
+            	var li = $("<li class='"+n.tagName+"'>"+_n.text()+"</li>");
             	li.on("click",function(argument) {
             		$(".selected",lis).removeClass("selected");
             		li.addClass("selected");
